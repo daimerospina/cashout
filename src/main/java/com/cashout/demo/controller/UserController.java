9dc530091d94461946341ef15e8948be2122cb73
Daimer Ospina <ddospina@bancolombia.com.co>
2024-07-31 07:30:46 -0500

    Adding update balance

--- src/main/java/com/cashout/demo/controller/UserController.java
+++ src/main/java/com/cashout/demo/controller/UserController.java
@@ -1,8 +1,10 @@
 package com.cashout.demo.controller;
 
+import com.cashout.demo.domain.Balance;
 import com.cashout.demo.domain.User;
 import com.cashout.demo.repository.UserRepository;
 import com.cashout.demo.service.IUserRestClient;
+import com.fasterxml.jackson.databind.introspect.TypeResolutionContext;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Mono;
@@ -25,6 +27,15 @@ public class UserController {
     @GetMapping("/{id}")
     public Mono<User> getUser(@PathVariable String id){
         return userRepository.findById(id);
+    }
+
+    @PutMapping("/{id}/balance")
+    public Mono<User> updateBalance(@PathVariable String id, @RequestBody Balance balance){
+        return userRepository.findById(id)
+                .flatMap(user -> {
+                    user.setBalance(user.getBalance() + balance.getBalance());
+                    return userRepository.save(user);
+                });
     }
 
 //    @PostMapping("/self")
---
+++ src/main/java/com/cashout/demo/domain/Balance.java
@@ -0,0 +1,12 @@
+package com.cashout.demo.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Balance {
+    private long balance;
+}
